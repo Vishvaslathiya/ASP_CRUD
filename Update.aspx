b440e0fc0cbe60ae13d86fd4d11bdc5000cad958
Vishvas Lathiya <111498112+Vishvaslathiya@users.noreply.github.com>
2024-03-29 01:46:30 +0530

    Update CRUD

---
+++ Update.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Update.aspx.cs" Inherits="Update" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <div style="align-items: center; padding: 5px; box-sizing: border-box; color: aliceblue; background-color: red">
+                <asp:TextBox ID="ename" runat="server" placeholder="Enter New Name : "></asp:TextBox>
+                <br />
+                <asp:TextBox ID="eemail" runat="server" placeholder="Enter New Email : "></asp:TextBox>
+                <br />
+                <asp:TextBox ID="epass" runat="server" placeholder="Enter New Password : "></asp:TextBox>
+                <br />
+                <asp:RadioButton ID="male" runat="server" Text="Male" />
+                <asp:RadioButton ID="female" runat="server" Text="female" />
+                <br />
+                <asp:Button ID="update" runat="server" Text="Update" OnClick="update_Click" />
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Update.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Update : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+        }
+    }
+
+    private void Updatedata()
+    {
+        String nname = ename.Text;
+        String nemail = eemail.Text;
+        String npass = epass.Text;
+        String ngen = Findgender();
+
+        int id = Convert.ToInt32(Request.QueryString["uid"]);
+
+        string connectionString = "Data Source=VISHVAS;Initial Catalog=Friday;Integrated Security=True;";
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            con.Open();
+            SqlCommand updateCmd = new SqlCommand("update tbl_student set name = @Name, email = @Email, pass = @Password, gender = @Gender where id = @ID", con);
+
+            // Add parameters to the SqlCommand to prevent SQL injection
+            updateCmd.Parameters.AddWithValue("@Name", nname);
+            updateCmd.Parameters.AddWithValue("@Email", nemail);
+            updateCmd.Parameters.AddWithValue("@Password", npass);
+            updateCmd.Parameters.AddWithValue("@Gender", ngen);
+            updateCmd.Parameters.AddWithValue("@ID", id);
+
+            updateCmd.ExecuteNonQuery();
+        }
+
+        Response.Write("<script>alert('Data Updated')</script>");
+    }
+
+
+    private string Findgender()
+    {
+        if(male.Checked)
+        {
+            return "Male";
+        }
+        else if (female.Checked)
+        {
+            return "Female";
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    protected void update_Click(object sender, EventArgs e)
+    {
+        Updatedata();
+    }
+}
