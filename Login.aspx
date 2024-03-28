416e60451a44de21f371c4c5745911c46d29d4d0
Vishvas Lathiya <111498112+Vishvaslathiya@users.noreply.github.com>
2024-03-28 23:54:57 +0530

    Login

---
+++ Login.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server" ID="logemail" placeholder="Enter Email" />
+            <br />
+            <asp:TextBox ID="logpassword" runat="server" placeholder="Enter Password"></asp:TextBox>
+            <br />
+            <asp:Button ID="login" runat="server" Text="Login" OnClick="login_Click" />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void register_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void login_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection("Data Source=VISHVAS;Initial Catalog=Friday;Integrated Security=True;");
+        con.Open();
+        String logEmail = logemail.Text;
+        String logPass = logpassword.Text;
+        String loginQuery = "select * from tbl_student where email = '" + logEmail + "' and pass = '" + logPass + "' ;";
+        SqlCommand cmd = new SqlCommand(loginQuery,con);
+        int isloged = (int)cmd.ExecuteScalar();
+
+        if (isloged>0)
+        {
+
+            Response.Write("<script>alert('Login Successful')</script>");
+            Response.Redirect("Dashboard.aspx");
+        }
+        else
+        {
+            Response.Write("<script>alert('Login Failed')</script>");
+        }
+        con.Close();
+
+    }
+}
