35ba70f0e774305521dd95f3a1877c0830f7a8bd
Vishvas Lathiya <111498112+Vishvaslathiya@users.noreply.github.com>
2024-03-28 23:54:33 +0530

    Registration

---
+++ Register.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Register.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="align-items:center; padding:5px; box-sizing:border-box; color:aliceblue; background-color:red ">
+            <asp:TextBox ID="name" runat="server" placeholder="Enter Name : "></asp:TextBox>
+            <br />
+            <asp:TextBox ID="email" runat="server" placeholder="Enter Email : "></asp:TextBox>
+            <br />
+            <asp:TextBox ID="pass" runat="server" placeholder="Enter Password : "></asp:TextBox>
+            <br />
+            <asp:RadioButton ID="male" runat="server" Text="Male" />
+            <asp:RadioButton ID="female" runat="server" Text="female" />
+            <br />
+            <asp:Button ID="register" runat="server" Text="Register Now" OnClick="register_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Register.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void register_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection("Data Source=VISHVAS;Initial Catalog=Friday;Integrated Security=True;");
+        con.Open();
+        String sname = name.Text;
+        String semail = email.Text;
+        String spass = pass.Text;
+        String gen = Findgender();
+        String insertQuery = "insert into tbl_student values( '"+sname+"' , '"+semail+"','"+spass + "','"+gen+"')";
+        SqlCommand cmd = new SqlCommand(insertQuery, con);
+        cmd.ExecuteNonQuery();
+        Response.Write("<script>alert('Registration Successful')</script>");
+        con.Close();
+
+        Response.Redirect("Login.aspx");
+
+    }
+    protected String Findgender()
+    {
+        if (male.Checked)
+        {
+            return male.Text;
+        }
+        else if(female.Checked)
+        {
+            return female.Text;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
