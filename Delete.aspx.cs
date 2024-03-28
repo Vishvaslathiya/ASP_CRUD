a99fc0c7939ba93ca8bb8be7c4e2da68e0a512c4
Vishvas Lathiya <111498112+Vishvaslathiya@users.noreply.github.com>
2024-03-29 01:46:01 +0530

    Delete CRUD

---
+++ Delete.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Delete : System.Web.UI.Page
+{
+    String conString = "Data Source=VISHVAS;Initial Catalog=Friday;Integrated Security=True;";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if(!IsPostBack)
+        {
+            int delid = Convert.ToInt32(Request.QueryString["did"]);
+
+            if (delid != 0)
+            {
+                DropUser();
+            }
+        }
+    }
+
+    private void DropUser()
+    {
+        int delid = Convert.ToInt32(Request.QueryString["did"]);
+
+        using (SqlConnection con = new SqlConnection(conString))
+        {
+            con.Close();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from tbl_student where id = "+ delid +";",con);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                Response.Write("<script>alert('User Dropped')</script>");
+                Response.Redirect("Dashboard.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('NOT Dropped')</script>");
+                Response.Redirect("Dashboard.aspx");
+            }
+        }
+        
+        
+    }
+}
